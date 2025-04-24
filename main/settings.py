278abbe9e73d538d1b22ffd77fbed22d91fc6d2c
Mary Rose Allen Y. Quijano <maryroseallenq@gmail.com>
2025-04-24 15:33:31 +0800

    Add backend-bwoh.onrender.com to ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://backend-bwoh.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
