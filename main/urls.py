885f61762684effb17bba4aa7803d8275103a35a
Mary Rose Allen Y. Quijano <maryroseallenq@gmail.com>
2025-04-26 22:55:30 +0800

    Can't see images

--- main/urls.py
+++ main/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # In production, explicitly serve media files
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
