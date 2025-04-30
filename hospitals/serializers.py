d451b480388bde5beec9fbeeeafb8b7125fe69e0
Mary Rose Allen Y. Quijano <maryroseallenq@gmail.com>
2025-04-30 14:17:51 +0800

    Iamges to see

--- hospitals/serializers.py
+++ hospitals/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Hospital
 
 class HospitalSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True, required=False)  # Returns full URL
+
     class Meta:
         model = Hospital
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'location', 'image', 'contact_number']
