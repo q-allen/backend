056a5081c66fa858e27b6c858ce951f5c9f63b28
Mary Rose Allen Y. Quijano <maryroseallenq@gmail.com>
2025-05-03 13:31:43 +0800

    Image

--- main/settings.py
+++ main/settings.py
@@ -10,12 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-^10aeas6j^pvjsz4cs_)69jesnw)0cbf^srn^j5hw_eoeqn@%f'
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^10aeas6j^pvjsz4cs_)69jesnw)0cbf^srn^j5hw_eoeqn@%f'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = [
     'backend-bwoh.onrender.com',
